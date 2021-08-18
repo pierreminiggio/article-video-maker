122e3df2092b903d56aa81e8037bdb2bc14ddfbb
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-08-19 00:51:49 +0200

    fix props

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,5 +1,6 @@
 import {Composition, getInputProps} from 'remotion';
 import {ArticleVideo} from './ArticleVideo';
+import cueDisplayTime from './Config/cueDisplayTime';
 import introLength from './Config/introLength';
 import { getDurationInFrames } from './Service/AudioContentDurationCalculator';
 
@@ -12,8 +13,13 @@ export const RemotionVideo: React.FC = () => {
 			<Composition
 				id="ArticleVideo"
 				component={ArticleVideo}
-				durationInFrames={(parseFloat(durationInSeconds) > 0 ? getDurationInFrames(parseFloat(durationInSeconds), fps) : 12691) + introLength}
-				fps={fps}
+				durationInFrames={
+					(parseFloat(durationInSeconds) > 0 ?
+						getDurationInFrames(parseFloat(durationInSeconds), fps)
+						: 12375
+					) + introLength + cueDisplayTime
+				}
+				fps={parseInt(fps) > 0 ? parseInt(fps) : 60}
 				width={1920}
 				height={1080}
 				defaultProps={{
