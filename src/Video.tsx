3a50b6cf12e66855f8a5d366e4a1708ca866a724
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-08-19 00:40:51 +0200

    get duration in frames

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,14 +1,17 @@
-import {Composition} from 'remotion';
+import {Composition, getInputProps} from 'remotion';
 import {ArticleVideo} from './ArticleVideo';
 import introLength from './Config/introLength';
 
 export const RemotionVideo: React.FC = () => {
+
+	const {durationInFrames} = getInputProps()
+
 	return (
 		<>
 			<Composition
 				id="ArticleVideo"
 				component={ArticleVideo}
-				durationInFrames={12691 + introLength}
+				durationInFrames={(parseInt(durationInFrames) > 0 ? parseInt(durationInFrames) : 12691) + introLength}
 				fps={60}
 				width={1920}
 				height={1080}
